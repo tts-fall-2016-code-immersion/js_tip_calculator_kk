d69c89418077bf7cdb9ce0c3552cf9989bf83e7e
Katie <newkatiek@gmail.com>
2016-10-18 21:53:58 -0400

    initial tip calc in js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var totalBill = parseFloat(prompt("How much is your bill?"));
+var billWithTip = parseFloat(totalBill + (totalBill * .20));
+console.log(billWithTip);
+alert("Your bill with a 20% tip is $" + billWithTip);
